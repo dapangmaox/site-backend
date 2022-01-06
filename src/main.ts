b64c4efcf90cbac982cf3bd2e3c1e7b6bc4c9fb9
wolf <liangxwei1030@163.com>
2022-01-07 00:44:04 +0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('大胖猫')
+    .setDescription('My Site API')
+    .setVersion('1.0')
+    .addTag('dapangmaox')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
