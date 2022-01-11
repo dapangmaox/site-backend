49ef20ed32aaaff485a7474740ae3831db32f455
liangxwei <liangxwei1030@163.com>
2022-01-11 23:32:56 +0800

    add feature modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { ArticleModule } from './article/article.module';
+import { UserModule } from './user/user.module';
+import { CategoryModule } from './category/category.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), CatsModule, ArticleModule, UserModule, CategoryModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
