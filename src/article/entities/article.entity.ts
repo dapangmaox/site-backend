fe3994db61ab3184a295af3f662645205b24ddca
liangxwei <liangxwei1030@163.com>
2022-02-01 12:59:59 +0800

    add create and update date in article

--- src/article/entities/article.entity.ts
+++ src/article/entities/article.entity.ts
@@ -1,6 +1,7 @@
 import { Category } from 'src/category/entities/category.entity';
 import { Tag } from 'src/tag/entities/tag.entity';
 import {
+  BeforeUpdate,
   Column,
   Entity,
   JoinColumn,
@@ -37,4 +38,15 @@ export class Article {
   @ManyToMany(() => Tag, (tag) => tag.articles)
   @JoinTable()
   tags: Tag[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updated: Date;
+
+  @BeforeUpdate()
+  setUpdated() {
+    this.updated = new Date();
+  }
 }
