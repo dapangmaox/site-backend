b5813e427a7234f8e964998a900e23c651ebc9a1
liangxwei <liangxwei1030@163.com>
2022-02-01 16:26:05 +0800

    add response interceptor

---
+++ src/interceptors/transform.interceptor.ts
@@ -0,0 +1,36 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Response as ExpressResponse } from 'express';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Response<T> {
+  statusCode: number;
+  message: string;
+  data: T;
+}
+
+@Injectable()
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => ({
+        statusCode: context.switchToHttp().getResponse<ExpressResponse>()
+          .statusCode,
+        message:
+          context.switchToHttp().getResponse<ExpressResponse>().statusMessage ||
+          'success',
+        data,
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new TransformInterceptor());
   app.enableCors();
 
   const swaggerConfig = new DocumentBuilder()
